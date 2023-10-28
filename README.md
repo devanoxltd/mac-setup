a56f5cc46d1d84459b11e6f36402d7ed441095ee
Chetan <contact@mrchetan.com>
2023-10-29 00:42:59 +0530

    Fix: Valet Install #1 and Improve Script

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # 🗣 Setup Your mac for laravel and flutter development
 
-### 💪   Follow Below steps
-
 ### 📝  Install Xcode
 ```sh
 xcode-select --install
@@ -12,53 +10,12 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 ```
 
-### 💻 Install Apps via script
+### 💪 💻 Start Setup Your mac for laravel and flutter development
 ```sh
 curl -s https://raw.githubusercontent.com/devanoxLtd/mac-setup/main/setup.sh | bash
 ```
 
-### 💻 Install Flutter
-```sh
-cd ~
-mkdir development/sdk
-cd development/sdk
-git clone https://github.com/flutter/flutter.git -b stable
-``` 
-
-####  💻 Install cocoapods for flutter
-```sh
-sudo gem install cocoapods
-```
-
-####  💻 Install Oh My Zsh auto suggestions
-
-Clone this repository into `$ZSH_CUSTOM/plugins` (by default `~/.oh-my-zsh/custom/plugins`)
-
-```sh
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-```
-
-####  💻 Install Oh My Zsh syntax highlighting
-
-Clone this repository into `$ZSH_CUSTOM/plugins` (by default `~/.oh-my-zsh/custom/plugins`)
-
-```sh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-```
-
-#### Replace zshrc file
-zshrc file is located at `~/.zshrc`
-
-```sh
-curl curl -s https://raw.githubusercontent.com/devanoxLtd/mac-setup/main/zshrc > ~/.zshrc
-```
-
-#### run flutter doctor
-```sh
-flutter doctor
-```
-
-##  ⚡ Configure Git Profile 
+##  ⚡ Configure Git Profile
 
 ```sh
 git config --global user.name "Mr Chetan"
--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install brew packages
 brew install --cask google-chrome
 brew install --cask anydesk
 brew install --cask vlc
@@ -22,6 +27,7 @@ brew tap nicoverbruggen/homebrew-cask
 
 brew install --cask phpmon
 
+# Install composer packages
 composer global require squizlabs/php_codesniffer:dev-master
 composer global require mrchetan/php_standard
 
@@ -29,7 +35,24 @@ composer global require laravel/installer
 
 composer global require laravel/valet
 
+# Install oh-my-zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Flutter
+osascript -e 'tell application "Terminal" to do script "curl -s https://raw.githubusercontent.com/devanoxLtd/mac-setup/main/install-flutter.sh | bash"'
+
+# Replace zshrc file
+# zshrc file is located at ~/.zshrc
+curl curl -s https://raw.githubusercontent.com/devanoxLtd/mac-setup/main/zshrc > ~/.zshrc
+
 # valet install
 osascript -e 'tell application "Terminal" to do script "valet install"'
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# set git config
+git config --global user.name "Mr Chetan"
+git config --global user.email "contact@mrchetan.com"
+
+# run flutter doctor
+osascript -e 'tell application "Terminal" to do script "flutter doctor"'
