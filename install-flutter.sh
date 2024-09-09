2f50bfaf02eede3ccde1348afa127afa17e7d346
Chetan <contact@mrchetan.com>
2024-09-09 14:05:06 +0530

    Update install-flutter.sh to install cocoapods, run flutter doctor, and accept licenses

--- install-flutter.sh
+++ install-flutter.sh
@@ -12,5 +12,13 @@ source ~/.zshrc
 # Install cocoapods for flutter
 sudo gem install cocoapods
 
+# Open android studio and install command line tools from the SDK manager
+
 # run flutter doctor
+flutter doctor
+
+# Accept licenses
+flutter doctor --android-licenses
+
+# recheck flutter doctor
 flutter doctor
